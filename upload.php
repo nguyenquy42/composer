5f23297f1ca426f94a010effceec40ffc3eb5b29
nguyenquy42 <nguyenquy26012014@gmail.com>
2020-08-11 13:25:00 +0700

    up no 2

--- upload.php
+++ upload.php
@@ -1,21 +1,9 @@
+<?php
+namespace bvq1212\SayHello;
+class SayHello
 {
-    "name": "nguyenquy42/hello-world",
-    "description": "the first composer package",
-    "type": "php",
-    "license": "MIT",
-    "authors": [
-        {
-            "name": "nguyenquy42",
-            "email": "nguyenquy26012014@gmail.com"
-        }
-    ],
-    "minimum-stability": "dev",
-    "require": {
-        "php": ">=5.3.0",
-    },
-    "autoload": {
-        "psr-4": {
-            "nguyenquy42\\SayHello\\": "src/"
-        }
+    public static function world()
+    {
+        return 'Hello World, Composer!';
     }
 }
